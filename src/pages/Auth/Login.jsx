ac0575f3345c58e7259cf82ffe9f2032afb64aab
Huski-commando <gyan.mu@gmail.com>
2024-01-10 16:17:33 +0530

    working on auth section-login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -6,15 +6,28 @@ import { motion } from "framer-motion";
 import { TextField } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { DevTool } from "@hookform/devtools";
-import { formVariant, imageVariant } from "@/utilities";
+import { formVariant, imageVariant } from "../../utilities";
 import { toast } from "react-toastify";
 
 // DEFINING THE FORM SCHEMA
 const schema = yup.object({
-  email: yup.string().email("Email format is not valid"),
+  email: yup
+    .string()
+    .email("Email format is not valid")
+    .required("Email is required!"),
+  password: yup.string().required("Password is required").min(8),
 });
 
 const Login = () => {
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { register, control, formState, handleSubmit } = form;
+  const { errors, isSubmitting } = formState;
+  
   return <div>Login</div>;
 };
 
